84cb7245930e6115ee54987de9b0905ed33f933c
zhangchunyang <zhangchunyang_pri@126.com>
2019-05-31 23:14:43 +0800

    travis ci test

--- nlpsc/test/test_transformer.py
+++ nlpsc/test/test_transformer.py
@@ -3,9 +3,8 @@ from nlpsc.representation.ernie.transformer import ErnieExtractEmbeddingTransfor
 from nlpsc.document import Document
 
 
-d = Document(text='我是正好洛杉矶格拉斯哥', lang='zh')
-print(ErnieExtractEmbeddingTransformer(vocab_path='default/ernie/vocab.txt').document2input(d))
-print(SequenceLabelTransformer(vocab_path='default/ernie/vocab.txt',
-                               label_map_config='default/ernie/label_map.json').document2input(d))
-
-print(ErnieClassifyTransformer(vocab_path='default/ernie/vocab.txt').document2input(d))
+def test_transformer():
+    d = Document(text='我是正好洛杉矶格拉斯哥', lang='zh')
+    print(ErnieExtractEmbeddingTransformer().document2input(d))
+    print(SequenceLabelTransformer(label_map_config='../default/ernie/label_map.json').document2input(d))
+    print(ErnieClassifyTransformer(vocab_path='../default/ernie/vocab.txt').document2input(d))
